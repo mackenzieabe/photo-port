179366b6d5af6f24b165c8f9e3e2c44ccdac3011
mackgabe <mackenzieabe@gmail.com>
2022-09-07 10:15:45 -0400

    added usestate and useeffect hooks

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 //This App.js file is the center of the application. Think of App.js as the root component, or the wrapper component that houses all of the other components. 
 //To effect any change on the application, we need to either modify this file or add components inside it.
 
-import React from 'react';
+import React, { useState } from 'react';
 import About from './components/About';
 import Nav from './components/Nav';
+import Gallery from './components/Gallery';
 
 function App() {
+  const [categories] = useState([
+    {
+      name: 'commercial',
+      description: 'Photos of grocery stores, food trucks, and other commercial projects',
+    },
+    { name: 'portraits', description: 'Portraits of people in my life' },
+    { name: 'food', description: 'Delicious delicacies' },
+    { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
+  ]);
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
   return (
     <div>
-      <Nav />
+      <Nav
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+      ></Nav>
+
       <main>
+        <Gallery></Gallery>
         <About></About>
       </main>
     </div>
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,53 +1,57 @@
-import React from 'react';
+import React, { useState } from "react";
+//The main difference between useEffect and useState is that useEffect is an API that reflects the lifecycle methods of the component, such as when the component mounts, unmounts, or updates. 
+import React, { useEffect } from 'react';
+import { capitalizeFirstLetter } from "../../utils/helpers";
 
+function Nav(props) {
+    const {
+        categories = [],
+        setCurrentCategory,
+        currentCategory,
+    } = props;
+    useEffect(() => {
+        document.title = capitalizeFirstLetter(currentCategory.name);
+    }, [currentCategory]);
+    //Notice in the preceding code block that the first argument is the callback function, and the second argument is an array with a single element, currentCategory. The second argument directs the hook to re-render the component on changes to the value of this state. In other words, if currentCategory changes now, the component will re-render so that the change in document.title will be visible to the user.
 
-const Nav = () => {
-    const categories = [
-        {
-            name: "commercial",
-            description:
-                "Photos of grocery stores, food trucks, and other commercial projects",
-        },
-        { name: "portraits", description: "Portraits of people in my life" },
-        { name: "food", description: "Delicious delicacies" },
-        {
-            name: "landscape",
-            description: "Fields, farmhouse, waterfalls, and the beauty of nature",
-        },
-    ];
-    function categorySelected(name) {
-        console.log(`${name} clicked`)
-    }
     return (
-        <header>
+        <header className="flex-row px-1">
             <h2>
-                <a data-testid= "link" href="/">
-                    <span role="img" aria-label="camera"> 📸</span> Oh Snap!
+                <a data-testid="link" href="/">
+                    <span role="img" aria-label="camera">
+                        {" "}
+                        📸
+                    </span>{" "}
+                    Oh Snap!
                 </a>
             </h2>
             <nav>
                 <ul className="flex-row">
                     <li className="mx-2">
-                        <a data-testid="about" href="#about">
-                            About Me
+                        <a
+                            href="#about"
+                        >
+                            About me
                         </a>
                     </li>
                     <li>
                         <span>Contact</span>
                     </li>
                     {categories.map((category) => (
-                        <li
-                            className="mx-1"
-                            key={category.name}>
-
-                            <span onClick = {() => categorySelected(category.name)}>
-                                {category.name}
+                        <li className={`mx-1 ${currentCategory.name === category.name && 'navActive'
+                            //The preceding line of code means that currentCategory.name === category.name will get evaluated, and as long as it is true, then the second bit of the short circuit, navActive, will be returned.
+                            }`} key={category.name}>
+                            <span
+                                onClick={() => {
+                                    setCurrentCategory(category)
+                                }}
+                            >
+                                {capitalizeFirstLetter(category.name)}
                             </span>
                         </li>
                     ))}
                 </ul>
             </nav>
-
         </header>
     );
 }
