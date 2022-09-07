772b547d86c33de925e4212287554d8c342a6c5f
mackgabe <mackenzieabe@gmail.com>
2022-09-07 16:18:30 -0400

    tests keep failing

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@
 //To effect any change on the application, we need to either modify this file or add components inside it.
 
 import React, { useState } from 'react';
-import About from './components/About';
 import Nav from './components/Nav';
+import About from './components/About';
 import Gallery from './components/Gallery';
 
 function App() {
@@ -15,7 +15,7 @@ function App() {
     { name: 'portraits', description: 'Portraits of people in my life' },
     { name: 'food', description: 'Delicious delicacies' },
     { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
-  ]);
+  ]); 
 
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
 
@@ -26,9 +26,8 @@ function App() {
         setCurrentCategory={setCurrentCategory}
         currentCategory={currentCategory}
       ></Nav>
-
       <main>
-        <Gallery></Gallery>
+        <Gallery currentCategory={currentCategory}></Gallery>
         <About></About>
       </main>
     </div>
