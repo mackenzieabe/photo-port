eab7a2b97ad92ea12907c0f9d85800a5a69b1d96
mackgabe <mackenzieabe@gmail.com>
2022-09-08 16:12:16 -0400

    created contact component test

--- src/components/Nav/_tests_/index.test.js
+++ src/components/Nav/_tests_/index.test.js
@@ -6,6 +6,24 @@ import Nav from '..';
 
 afterEach(cleanup);
 
+const categories = [
+  { name: 'portraits', description: 'Portraits of people in my life' }
+]
+const mockCurrentCategory = jest.fn();
+const mockSetCurrentCategory = jest.fn();
+const mockContactSelected = jest.fn();
+const mockSetContactSelected = jest.fn(); 
+
+it('renders', () => {
+  render(<Nav
+    categories={categories}
+    setCurrentCategory={mockSetCurrentCategory}
+    currentCategory={mockCurrentCategory}
+    contactSelected={mockContactSelected}
+    setContactSelected={mockSetContactSelected}
+  />);
+})
+
 describe('Nav component', () => {
   it('renders', () => {
     render(<Nav />);
